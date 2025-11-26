a35fdb080e8b68e2d59ed1548df9c6a29d323ae9
Akifhan Ilgaz <akifhanilgaz@gmail.com>
2025-11-26 14:15:30 +0300

    delete

--- src/features/landing/components/hero.tsx
+++ src/features/landing/components/hero.tsx
@@ -1,11 +1,3 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/src/shared/config/site";
-import { title, subtitle } from "@/src/shared/components/primitives";
-import { GithubIcon } from "@/src/shared/components/icons";
 import { Button } from "@heroui/button";
 import { CheckCircle } from "lucide-react";
 
